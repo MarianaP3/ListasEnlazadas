b1b0d4c74d51dbef02e276c067bfbfd7a1955128
MarianaP3 <101375393+MarianaP3@users.noreply.github.com>
2023-03-03 21:56:39 +0000

    REVISA CONJUNTOS.C

--- conjuntos.c
+++ conjuntos.c
@@ -10,12 +10,49 @@ typedef struct conjunto{
     TNodo *cab;
     unsigned int tam;
 }TConjunto;
+TNodo *CreaNodo(int Dato);
 void inicializar(TConjunto *Conj);
-bool agregar(TConjunto *Conj, int dato);
+bool agregar(TConjunto **Conj, int dato);
 bool eliminar(TConjunto *Conj, int dato);
 bool existe(TConjunto *Conj, int dato);
 size_t cardinalidad(TConjunto *Conj);
 
 int main(){
     printf("Hello World\n");
+}
+
+TNodo *CreaNodo(int Dato){
+    TNodo *nodo=(TNodo*)malloc(sizeof(TNodo));
+    if(nodo==NULL){
+        printf("Error de memoria\n");
+        exit(EXIT_FAILURE);
+    }
+    nodo->dato=Dato;
+    nodo->sig=NULL;
+    return nodo;
+}
+void inicializar(TConjunto *Conj){
+    Conj->cab=NULL;
+}
+bool agregar(TConjunto **Conj, int dato){
+    TNodo *nuevo_nodo=CreaNodo(dato);
+    if((*Conj)->cab==NULL){
+        (*Conj)->cab=nuevo_nodo;
+        nuevo_nodo=NULL;
+        (*Conj)->tam=((*Conj)->tam)+1;
+        return true;
+    }
+    if((((*Conj)->cab)->dato)>dato){//agrega al inicio
+        nuevo_nodo->sig=(*Conj)->cab;
+        (*Conj)->cab=nuevo_nodo;
+        ((*Conj)->tam)++;
+    }
+    TNodo *previo=(*Conj)->cab;
+    TNodo *ultimo=(*Conj)->cab->sig;
+    while(ultimo->sig!=NULL){
+        if(dato<ultimo->sig->dato){
+            penultimo=nuevo_nodo;
+            nuevo_nodo=ultimo;
+        }
+    }
 }
